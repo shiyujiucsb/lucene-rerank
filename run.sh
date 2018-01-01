12c59ccbbb64f3065f9dce474cffb767cfda24e9
shiyu <shiyu@cs.ucsb.edu>
2018-01-01 12:23:54 -0800

    .

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export CLASSPATH=.:./lucene-solr/lucene/build/core/classes/java/:./lucene-solr/lucene/build/queryparser/classes/java/
+export CLASSPATH=.:./lucene-solr/lucene/build/core/classes/java/:./lucene-solr/lucene/build/queryparser/classes/java/:./lucene-solr/lucene/build/analysis/common/classes/java/
 
 javac buildIndex.java searchIndex.java
 
